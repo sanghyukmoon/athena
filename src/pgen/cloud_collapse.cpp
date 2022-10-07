86c57e79e5484c311b516770c4e114e05d4f8fdd
Sanghyuk Moon <sanghyuk.moon@princeton.edu>
2022-10-07 11:53:37 -0400

    Add Jeans condition to cloud-collapse problem.

--- src/pgen/cloud_collapse.cpp
+++ src/pgen/cloud_collapse.cpp
@@ -35,18 +35,31 @@
 #error "This problem generator requires Multigrid gravity solver."
 #endif
 
+
+int JeansCondition(MeshBlock *pmb);
+Real njeans;
+
 //========================================================================================
 //! \fn void Mesh::InitUserMeshData(ParameterInput *pin)
 //  \brief
 //========================================================================================
 void Mesh::InitUserMeshData(ParameterInput *pin) {
-  SetFourPiG(4*PI*1.0); // G = 1
+  if (NON_BAROTROPIC_EOS) {
+    std::stringstream msg;
+    msg << "This problem generator does not support adiabatic EOS." << std::endl;
+    ATHENA_ERROR(msg);
+    return;
+  }
+
+  // In the unit system where [L] = L_J, [M] = M_J, [T] = t_ff,
+  // the gravitational constant becomes G = 3*PI/32.
+  SetFourPiG(4.*PI*(3.*PI/32.));
 
   // turb_flag is initialzed in the Mesh constructor to 0 by default;
   // turb_flag = 1 for decaying turbulence
   // turb_flag = 2 for impulsively driven turbulence
   // turb_flag = 3 for continuously driven turbulence
-  turb_flag = pin->GetInteger("problem","turb_flag");
+  turb_flag = pin->GetInteger("turbulence","turb_flag");
   if (turb_flag != 0) {
 #ifndef FFT
     std::stringstream msg;
@@ -57,11 +70,9 @@ void Mesh::InitUserMeshData(ParameterInput *pin) {
 #endif
   }
 
-  if (NON_BAROTROPIC_EOS) {
-    std::stringstream msg;
-    msg << "This problem generator does not support adiabatic EOS." << std::endl;
-    ATHENA_ERROR(msg);
-    return;
+  if (adaptive) {
+    njeans = pin->GetReal("mesh", "njeans");
+    EnrollUserRefinementCondition(JeansCondition);
   }
   return;
 }
@@ -90,4 +101,25 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
 //========================================================================================
 
 void Mesh::UserWorkAfterLoop(ParameterInput *pin) {
+}
+
+// Jeans Condition
+int JeansCondition(MeshBlock *pmb) {
+  Real njmin = 1e300;
+  const Real dx = pmb->pcoord->dx1f(0); // assuming uniform cubic cells
+  const Real cs = pmb->peos->GetIsoSoundSpeed();
+  const Real fac = std::sqrt(PI) / dx;
+  for (int k = pmb->ks; k<=pmb->ke; ++k) {
+    for (int j = pmb->js; j<=pmb->je; ++j) {
+      for (int i = pmb->is; i<=pmb->ie; ++i) {
+        Real nj = fac * cs / std::sqrt(pmb->phydro->w(IDN,k,j,i));
+        njmin = std::min(njmin, nj);
+      }
+    }
+  }
+  if (njmin < njeans)
+    return 1;
+  if (njmin > njeans * 2.5)
+    return -1;
+  return 0;
 }
